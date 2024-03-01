d3810660deacdd8cc13eb45b4c635fe3c29cb25f
nickola23 <nikolamilanovic2305@gmail.com>
2024-03-01 20:20:19 +0100

    groups page finished

--- groups.php
+++ groups.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/groups.css">
     <title>FitTrack Grupe</title>
 </head>
 <body>
@@ -15,18 +16,32 @@
     include('./php/konekcija.php');
 ?>
 <main>
-    <h1>Sve grupe</h1>
+    <section class="groupsSec">
+        <h1>Istrazite grupe</h1>
+        <div class="groupsCont">
+            <?php
+                $sql = "SELECT * FROM grupa";
+                $result = $conn->query($sql);
 
-    <?php
-        $sql = "SELECT * FROM grupa";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "<p>Naziv:" . $row["naziv"] . ", trener:" .  $row["trener"] . "</p>";
-            }
-        }
-    ?>
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo
+                        '<div class="groupMain">
+                            <img src="' .  $row["slika"] . '" alt="Slika grupe" />
+                            <div class="groupRight">
+                                <div class="groupRightText">
+                                    <h2>' . $row["naziv"] . '</h2>
+                                    <p>Trener: ' .  $row["trener"] . '</p>
+                                    <p>' .  $row["opis"] . '</p>
+                                </div> 
+                                <div class="btnDark">Pridruzi se</div>' .
+                            '</div>
+                        </div>';
+                    }
+                }
+            ?>
+        </div>
+    </section>
 </main>
 </body>
 </html>
--- membership.php
+++ membership.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/global.css">
-    <title>FitTrack Prijava</title>
+    <title>FitTrack Clanarina</title>
 </head>
 <body>
 <?php
@@ -67,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <main>
     <h1>Clanarina</h1>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" name="">  
-    Email: <input type="text" name="email">
+    Iznos za uplatu: <input type="text" name="email">
     <span class="error">* <?php echo $emailErr?></span>
     <br><br>
     Lozinka: <input type="password" name="lozinka">
